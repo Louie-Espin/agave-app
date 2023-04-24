bad3a64bdd6b3761de52d5d4df8dbb51e3d4d603
Louie-Espin <luisespinoza112599@gmail.com>
2023-04-24 15:49:37 -0700

    WIP Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+
+type LoaderProps = {
+
+}
+
+const Loader: FC<LoaderProps> = ({}) => {
+    return(
+        <div>
+            Loading...
+        </div>
+    );
+}
+
+export default Loader;
