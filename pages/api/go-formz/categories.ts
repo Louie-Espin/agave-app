5bbd44a0e5567e67900da87739a0ec77ce87d63e
Louie-Espin <luisespinoza112599@gmail.com>
2023-05-25 15:54:38 -0700

    Create categories.ts
    [WIP] initial work for Go Formz API

---
+++ pages/api/go-formz/categories.ts
@@ -0,0 +1,37 @@
+import type { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
+import axios from "axios";
+import { apiHandler } from "utils/api/apiHandler";
+import initAuth from "@firebaseUtils/initAuth";
+import initFirebaseAdminSDK from "@firebaseUtils/firebaseAdmin";
+
+const GET_URL: string = "https://accounts.goformz.com/v2/templates";
+
+const ENCODED: string = Buffer.from(`${process.env.GOFORMZ_LOGIN}:${process.env.GOFORMZ_PASS}`).toString('base64');
+
+const config = {
+    url: GET_URL,
+    method: 'GET',
+    headers: {
+        'Host': 'api.goformz.com',
+        'Connection': 'keep-alive',
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Authorization': `Basic ${ENCODED}`,
+    },
+};
+
+const getTemplates: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+    initAuth();
+    initFirebaseAdminSDK();
+
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ categories: null, message: "GET form categories failed; No token!" });
+
+    const getCategories = await axios(config);
+
+    return res.status(200).json({ categories: getCategories.data, message: "GET Categories success." });
+};
+
+export default apiHandler({
+    GET: getTemplates,
+});
