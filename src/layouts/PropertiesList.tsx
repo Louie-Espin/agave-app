274b84d2624908d4bd61e480df21c699e8f2c015
Louie-Espin <luisespinoza112599@gmail.com>
2023-07-05 16:13:53 -0700

    [WIP] Refactoring the Work History pages
    - Added new index page
    - Index page now shows user's properties
    - Moved old index page to /[id]/index
    - Refactored PropertiesList component to use Stack, added AddProperty component

--- src/layouts/PropertiesList.tsx
+++ src/layouts/PropertiesList.tsx
@@ -1,44 +1,71 @@
-import React, { FC } from 'react';
+import React, { FC, Fragment } from 'react';
 
-import {Box, CircularProgress, Paper, Skeleton} from '@mui/material';
+import {Box, BoxProps, CircularProgress, Stack, Skeleton, Paper, Button} from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
-import HomeWorkIcon from "@mui/icons-material/HomeWork";
-import { H2 } from "components/Typography";
+
+import { H2, Paragraph } from "components/Typography";
+import { NextLinkComposed } from "components/Link";
 
 import { PropertySchema } from "utils/api/yup";
 import * as yup from "yup";
 import PropertyCard from "components/PropertyCard";
+
+import HomeWorkIcon from "@mui/icons-material/HomeWork";
+import AgaveSVG from "@public/assets/agave-graphics/dark-green/agave2.svg";
 
 type PropertiesListProps = {
     properties?: yup.InferType<typeof PropertySchema>[],
     validating: boolean,
     loading: boolean,
     error: any,
+    showTitle?: boolean,
 }
-const PropertiesList: FC<PropertiesListProps> = ({ properties = [], validating, loading, error }) => {
-
-    if (loading) return(
-        <Skeleton variant='rounded' height={470} width={'100%'}/>
-    );
+const PropertiesList: FC<PropertiesListProps> = ({ properties = [], validating, loading, error, showTitle = true }) => {
 
     return(
-        <Box component={Paper} pt='1.5rem' pb='2.5rem' px='1.5rem'>
-            <Box display='flex' alignItems='center' my={2}>
+        <Fragment>
+            <Box display={ showTitle ? 'flex' : 'none' } alignItems='center' my={2}>
                 <HomeWorkIcon color="primary"/>
                 <H2 ml={1.5} my="0px" lineHeight="1" whiteSpace="pre">
                     Your Properties
                 </H2>
                 <CircularProgress size='1rem' sx={{ ml: 2, display: (validating ? 'flex' : 'none') }}/>
             </Box>
-            <Grid container spacing={2}>
+            <Stack direction='row' flexWrap='wrap' sx={{ gap: '1em' }} width={'100%'}>
                 {
-                    (properties.length) ? properties.map((p) =>
-                        <Grid xs={12} md={6} lg={6} key={p.id}>
-                            <PropertyCard propertyName={p.name} imageUrl={p.displayImage as string} propertyContent={p.address}/>
-                        </Grid>
-                    ) : <div>No Properties!</div>
+                    (!loading) ? properties.map((p) =>
+                        <PropertyCard key={p.id} propertyName={p.name} imageUrl={p.displayImage as string} propertyContent={p.address}
+                                      sx={{ flex: '1 1 100%', maxWidth: { xs: '100%', md: 'calc(50% - 1em)' } }}/>
+                    ) : <Skeleton variant='rounded' height={300}
+                                  sx={{ flex: '1 1 100%', maxWidth: { xs: '100%', md: 'calc(50% - 1em)' } }}/>
                 }
-            </Grid>
+                <AddProperty />
+            </Stack>
+        </Fragment>
+    );
+}
+
+const AddProperty: FC<BoxProps> = () => {
+
+    return(
+        <Box component={Paper} display={"flex"} p={3} alignItems={'center'}
+             sx={{
+                 flex: '1 1 100%',
+                 maxWidth: { xs: '100%', md: 'calc(50% - 1em)' },
+                 minHeight: '290px',
+                 position: 'relative',
+
+                 background: `url(${AgaveSVG.src}) right bottom 10px / auto 90% no-repeat, beige`,
+
+                 outline: '2px solid white',
+                 outlineOffset: '-10px',
+                 borderRadius: '5px',
+             }}
+        >
+            <Box ml={{ sm: 2 }}>
+                <H2 sx={{ color: '#871A78' }} mb={2}>{"Are we missing something?"}</H2>
+                <Button variant='contained' color='secondary' component={NextLinkComposed} to='/contact'>Contact Us</Button>
+            </Box>
         </Box>
     );
 }
