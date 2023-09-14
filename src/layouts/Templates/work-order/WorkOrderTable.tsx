fbb869619358d08d2dba75ce987cbbbb497a0b34
Louie-Espin <luisespinoza112599@gmail.com>
2023-09-14 00:36:48 -0700

    Update WorkOrderTable.tsx
    Removed Data Grid, simplified design

--- src/layouts/Templates/work-order/WorkOrderTable.tsx
+++ src/layouts/Templates/work-order/WorkOrderTable.tsx
@@ -1,84 +1,50 @@
-import {FC, useMemo } from 'react';
-import { Stack, Box, Paper, styled } from '@mui/material';
-import { DataGrid, GridColDef, GridColTypeDef } from '@mui/x-data-grid';
+import { FC, ReactNode } from 'react';
+import { Stack, StackProps} from '@mui/material';
+import { Span } from 'components/Typography';
+import { format } from "date-fns";
 
-const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
-    borderColor: theme.palette.grey["400"],
-    '& .MuiDataGrid-columnHeader': { backgroundColor: theme.palette.primary.light },
-    '& .MuiDataGrid-withBorderColor': { borderColor: theme.palette.grey["400"] }
-}))
+import HandymanOutlinedIcon from '@mui/icons-material/HandymanOutlined';
+import HomeRepairServiceOutlinedIcon from '@mui/icons-material/HomeRepairServiceOutlined';
+import SellOutlinedIcon from '@mui/icons-material/SellOutlined';
 
-const usdPrice: GridColTypeDef = {
-    type: 'number', flex: 1,
-    sortComparator: (v1, v2) => parseInt(v1.replace(/[^0-9.-]+/g,"")) - parseInt(v2.replace(/[^0-9.-]+/g,"")),
-};
+import BadgeOutlinedIcon from '@mui/icons-material/BadgeOutlined';
+import EventAvailableOutlinedIcon from '@mui/icons-material/EventAvailableOutlined';
+import NumbersOutlinedIcon from '@mui/icons-material/NumbersOutlined';
 
-const matCols: GridColDef[] = [
-    { field: 'material', headerName: 'Materials Or Service', flex: 2, sortable: false },
-    { field: 'costCode', headerName: 'Cost Code', flex: 1, sortable: false },
-    { field: 'unitType', headerName: 'Unit Type', flex: 1, sortable: false, align: 'center' },
-    { field: 'quantity', headerName: 'Quantity', type: 'number', flex: 1 },
-    { field: 'unitCost', headerName: 'Unit Cost', ...usdPrice },
-    { field: 'extension', headerName: 'Extension', ...usdPrice },
-];
-
-const labCols: GridColDef[] = [
-    { field: 'techName', headerName: 'Technician', flex: 1 },
-    { field: 'hours', headerName: 'Hours', flex: 1 },
-    { field: 'type', headerName: 'Type', flex: 1 },
-    { field: 'rate', headerName: 'Rate', flex: 1 },
-    { field: 'total', headerName: 'Total', flex: 1 },
-]
-
-type WorkOrderProps = { fields: any }
-const WorkOrderTable: FC<WorkOrderProps> = ({ fields }) => {
-
-    if (!fields) return null;
-
-    const matTable = fields?.['Materials Table']?.rows;
-    const labTable = fields?.['Labor Table']?.rows;
-
-    const grandTotal = fields?.['Grand Total']?.value;
-
-    const matEntries = Object.entries(matTable).map((i: Record<string, any>) => (
-        {
-            id: i[0],
-            material: i[1].columns['Materials or Service']?.value,
-            costCode: i[1].columns['Cost Code']?.text,
-            unitType: i[1].columns['Unit Type']?.text,
-            quantity: i[1].columns['Quantity']?.value,
-            unitCost: i[1].columns['Unit Cost']?.text,
-            extension: i[1].columns['Extension']?.text,
-        }
-    )).filter((i) => (i.extension !== "$0.00"));
-
-    const labEntries = Object.entries(labTable).map((i: Record<string, any>) => (
-        {
-            id: i[0],
-            techName: i[1].columns['Technician Name']?.value,
-            hours: i[1].columns['Hours']?.text,
-            type: i[1].columns['Type']?.text,
-            rate: i[1].columns['Rate']?.value,
-            total: i[1].columns['Total']?.text,
-        }
-    )).filter((i) => (i.total !== "$0.00"))
+type WorkOrderProps = {
+    materials: string | null, labor: string | null, total: string | null,
+    technician: string | null, date: string | null, jobNum: string | null,
+}
+const WorkOrderTable: FC<WorkOrderProps> = ({ materials, labor, total, technician, date, jobNum }: WorkOrderProps) => {
 
     return(
-        <Stack direction='row' flexWrap='wrap' position='relative' width='100%' sx={{ gap: '1em', my: '1em' }}>
-
-            <StyledDataGrid columns={matCols} rows={matEntries} showColumnVerticalBorder showCellVerticalBorder disableColumnMenu={true} />
-
-            <Stack direction='row' flexWrap='wrap' position='relative' width='100%' sx={{ gap: '1em' }}>
-
-                <StyledDataGrid columns={labCols} rows={labEntries} showColumnVerticalBorder showCellVerticalBorder />
-
-                <Box minWidth={{ xs: '100%', sm: 'calc(33% - 1em)' }} maxWidth={'100%'} position='relative'>
-                    {grandTotal}
-                </Box>
-
+        <Stack direction='column' position='relative' p={2} sx={{ gap: '2em' }}>
+            <Stack direction='row' flexWrap='wrap' justifyContent='space-around' sx={{ gap: '1em' }}>
+                <WorkOrderItem label={'Materials'} value={materials} icon={<HomeRepairServiceOutlinedIcon color={'disabled'} />} />
+                <WorkOrderItem label={'Labor'} value={labor} icon={<HandymanOutlinedIcon color={'disabled'} />} />
+                <WorkOrderItem label={'Total'} value={total} icon={<SellOutlinedIcon color={'disabled'} />} />
+            </Stack>
+            <Stack direction='row' flexWrap='wrap' justifyContent='space-around' sx={{ gap: '1em' }}>
+                <WorkOrderItem label={'Technician'} value={technician} icon={<BadgeOutlinedIcon color={'disabled'} />} />
+                <WorkOrderItem label={'Completed'} icon={<EventAvailableOutlinedIcon color={'disabled'} />}
+                               value={date ? format(new Date(date), 'MM/dd/yyyy') : null}
+                />
+                <WorkOrderItem label={'Job Number'} value={jobNum} icon={<NumbersOutlinedIcon color={'disabled'} />} />
             </Stack>
         </Stack>
     )
+}
+
+const WorkOrderItem: FC<{label: string, value: string | null, icon: ReactNode} & StackProps> = ({label, value = '---', icon, ...props}) => {
+    return(
+        <Stack flex='1 0' sx={{ gap: '0.5em' }} {...props}>
+            <Stack direction='row' flexWrap='nowrap' alignItems='center' sx={{ gap: '0.5em' }}>
+                {icon}
+                <Span color={'grey.600'}>{label}</Span>
+            </Stack>
+            <Span color={'grey.800'} fontWeight={'800'} fontSize={'1.3em'}>{value}</Span>
+        </Stack>
+    );
 }
 
 export default WorkOrderTable;
